1677c3fbfe6e97f0d6191428d0272f603615f4be
Moeed Sarwar <oyemoeed@gmail.com>
2023-12-11 18:25:09 +0500

    Created Phone Directory

---
+++ src/screens/phoneDirectory.tsx
@@ -0,0 +1,87 @@
+// PhoneDirectory.tsx
+import React, {useState, useEffect} from 'react';
+import {View, Text, TextInput, Button, FlatList} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+interface Contact {
+  id: string;
+  name: string;
+  phoneNumber: string;
+}
+
+const PhoneDirectory: React.FC = () => {
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [contacts, setContacts] = useState<Contact[]>([]);
+
+  useEffect(() => {
+    loadContacts();
+  }, []);
+
+  const saveContact = async () => {
+    try {
+      const newContact: Contact = {
+        id: String(Date.now()),
+        name,
+        phoneNumber,
+      };
+
+      await AsyncStorage.setItem(newContact.id, JSON.stringify(newContact));
+
+      setContacts(prevContacts => [...prevContacts, newContact]);
+
+      setName('');
+      setPhoneNumber('');
+    } catch (error) {
+      console.error('Error saving contact:', error);
+    }
+  };
+
+  const loadContacts = async () => {
+    try {
+      const keys = await AsyncStorage.getAllKeys();
+      const savedContacts = await AsyncStorage.multiGet(keys);
+
+      const loadedContacts: Contact[] = savedContacts.map(([key, value]) =>
+        JSON.parse(value),
+      );
+
+      setContacts(loadedContacts);
+    } catch (error) {
+      console.error('Error loading contacts:', error);
+    }
+  };
+
+  const renderItem = ({item}: {item: Contact}) => (
+    <View>
+      <Text>{item.name}</Text>
+      <Text>{item.phoneNumber}</Text>
+    </View>
+  );
+
+  return (
+    <View>
+      <Text>Add Contact</Text>
+      <TextInput
+        placeholder="Name"
+        value={name}
+        onChangeText={text => setName(text)}
+      />
+      <TextInput
+        placeholder="Phone Number"
+        value={phoneNumber}
+        onChangeText={text => setPhoneNumber(text)}
+      />
+      <Button title="Save Contact" onPress={saveContact} />
+
+      <Text>Contacts</Text>
+      <FlatList
+        data={contacts}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  );
+};
+
+export default PhoneDirectory;
